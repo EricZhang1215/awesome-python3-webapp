f81b2d985fafbc100d2b6cdc45f46e69aa718fb5
zhangqingsong <879691755@qq.com>
2018-09-14 10:34:02 +0800

    [+]set configuration，default for develop, override for product env

---
+++ www/config_override.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Override configurations. for product
+'''
+
+__author__ = 'EricZhang'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
